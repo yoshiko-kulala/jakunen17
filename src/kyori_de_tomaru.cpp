2f3c7ca26bc77f423bfdefb535775278f04420b3
Yano21210 <aichitead21210@gmail.com>
2022-07-17 19:52:21 +0900

    kadai1_17

--- src/kyori_de_tomaru.cpp
+++ src/kyori_de_tomaru.cpp
@@ -3,16 +3,33 @@
 #include <std_msgs/Int8.h>
 #include <std_msgs/Float32.h>
 using namespace std;
+#include <stdio.h>
 
 int robot_mode=0;
 int stage = 0;
 int cou=0;
 
-int f_kyori = 0;
+float f_kyori = -1;
+float b_kyori = -1;
+float r_kyori = -1;
+float l_kyori = -1;
+float sokudo = 0.2;
 
 void callback_range_front(const std_msgs::Float32& msg) {
     //ROS_INFO("/range_front\t:\t%f\n\n", msg.data);
     f_kyori =  msg.data;
+}
+void callback_range_back(const std_msgs::Float32& msg) {
+    //ROS_INFO("/range_back\t:\t%f\n\n", msg.data);
+     b_kyori =  msg.data;
+}
+void callback_range_right(const std_msgs::Float32& msg) {
+    //ROS_INFO("/range_right\t:\t%f\n\n", msg.data);
+     r_kyori =  msg.data;
+}
+void callback_range_left(const std_msgs::Float32& msg) {
+    //ROS_INFO("/range_left\t:\t%f\n\n", msg.data);
+    l_kyori =  msg.data;
 }
 
 void callback_robot_state(const std_msgs::Int8& msg) {
@@ -31,6 +48,9 @@ int main(int argc, char **argv){
   std_msgs::Int8 fin;
   ros::Subscriber sub_robot_state = nh.subscribe("/robot_state", 100, callback_robot_state);
   ros::Subscriber sub_range_front = nh.subscribe("/range_ahead", 100, callback_range_front);
+  ros::Subscriber sub_range_back = nh.subscribe("/range_back", 100, callback_range_back);
+  ros::Subscriber sub_range_right = nh.subscribe("/range_right", 100, callback_range_right);
+  ros::Subscriber sub_range_left = nh.subscribe("/range_left", 100, callback_range_left);
   while (ros::ok()) {
     switch(stage){
       case 0:
@@ -43,120 +63,163 @@ int main(int argc, char **argv){
         pub_twist.publish(vel);
         fin.data=0;
         pub_hand_task.publish(fin);
-        if(robot_mode==1)stage++;
+        //if(robot_mode==1)stage++;
         stage++;
-        break;
       case 1:
         cou++;
-        vel.linear.x = 0.02*cou;
+        vel.linear.x = sokudo;
         vel.linear.y = 0.0;
         vel.linear.z = 0.0;
         vel.angular.x = 0.0;
         vel.angular.y = 0.0;
         vel.angular.z = 0.0;
-        pub_twist.publish(vel);
-        if(cou>=10){
-          cou=0;
-          stage++;
-        }
-        stage++;
-        break;
-      case 2:
-        cou++;
-        vel.linear.x = 0.2;
-        vel.linear.y = 0.0;
-        vel.linear.z = 0.0;
-        vel.angular.x = 0.0;
-        vel.angular.y = 0.0;
-        vel.angular.z = 0.0;
-        pub_twist.publish(vel);
         // if(cou>=40){
-        if(f_kyori<0.3){
-          cou=0;
-          stage=8;//強制終了
+        if(f_kyori<0.6){
+          printf("aaaaa=\n");
+          vel.linear.x = 0.1;
         }
-        break;
-      case 3:
-        cou++;
-        vel.linear.x = 0.2-0.02*cou;
-        vel.linear.y = 0.0;
-        vel.linear.z = 0.0;
-        vel.angular.x = 0.0;
-        vel.angular.y = 0.0;
-        vel.angular.z = 0.0;
+        if(f_kyori<0.4 && f_kyori>0){
+          cou=0;
+          stage++;//強制終了
+        }
         pub_twist.publish(vel);
-        if(cou>=10){
-          cou=0;
-          stage++;
-        }
-        break;
-      case 4:
+        break; 
+    case 2:
         cou++;
         vel.linear.x = 0.0;
+        vel.linear.y = -1*sokudo;
+        vel.linear.z = 0.0;
+        vel.angular.x = 0.0;
+        vel.angular.y = 0.0;
+        vel.angular.z = 0.0;
+        //pub_twist.publish(vel);
+        // if(cou>=40){
+       if(r_kyori<0.6){
+          printf("aaaaa=\n");
+          vel.linear.y = -0.1;
+        }
+       if(r_kyori<0.4 && r_kyori>0){
+          cou=0;
+          stage++;//強制終了
+        }
+        //pub_twist.publish(vel);
+        break;
+    case 3:
+        cou++;
+        vel.linear.x = sokudo;
         vel.linear.y = 0.0;
         vel.linear.z = 0.0;
         vel.angular.x = 0.0;
         vel.angular.y = 0.0;
         vel.angular.z = 0.0;
-        pub_twist.publish(vel);
-        if(cou>=30){
-          cou=0;
-          stage++;
+        //pub_twist.publish(vel);
+        // if(cou>=40){
+        if(f_kyori<0.6){
+          printf("aaaaa=\n");
+          vel.linear.x = 0.1;
         }
+        if(f_kyori<0.4 && f_kyori>0){
+          cou=0;
+          stage++;//強制終了
+        }
+        pub_twist.publish(vel);
+        break;
+    case 4:
+        cou++;
+        vel.linear.x = 0.0;
+        vel.linear.y = sokudo;
+        vel.linear.z = 0.0;
+        vel.angular.x = 0.0;
+        vel.angular.y = 0.0;
+        vel.angular.z = 0.0;
+        //pub_twist.publish(vel);
+        // if(cou>=40){
+       if(l_kyori<0.6){
+          printf("aaaaa=\n");
+          vel.linear.y = 0.1;
+        }
+       if(l_kyori<0.4 && l_kyori>0){
+          cou=0;
+          stage++;//強制終了
+        }
+        pub_twist.publish(vel);
         break;
       case 5:
         cou++;
-        vel.linear.x = -0.02*cou;
-        vel.linear.y = 0.0;
+        vel.linear.x = 0.0;
+        vel.linear.y = -1*sokudo;
         vel.linear.z = 0.0;
         vel.angular.x = 0.0;
         vel.angular.y = 0.0;
         vel.angular.z = 0.0;
-        pub_twist.publish(vel);
-        if(cou>=10){
-          cou=0;
-          stage++;
+        // if(cou>=40){
+       if(l_kyori>1.3){
+          printf("aaaaa=\n");
+          vel.linear.y = -0.1;
         }
+       if(l_kyori>1.65 && l_kyori>0){
+        printf("tikai\n");
+          cou=0;
+          stage++;//強制終了
+        }
+        pub_twist.publish(vel);
         break;
       case 6:
         cou++;
-        vel.linear.x = -0.2;
+        vel.linear.x = -1*sokudo;
         vel.linear.y = 0.0;
         vel.linear.z = 0.0;
         vel.angular.x = 0.0;
         vel.angular.y = 0.0;
         vel.angular.z = 0.0;
-        pub_twist.publish(vel);
-        if(cou>=17){
+        //pub_twist.publish(vel);
+        // if(cou>=40){
+       if(b_kyori<0.6){
+          printf("aaaaa=\n");
+          vel.linear.x = -0.1;
+        }
+       if(b_kyori<0.4 && b_kyori>0){
           cou=0;
-          stage++;
+          stage++;//強制終了
         }
         break;
       case 7:
         cou++;
-        vel.linear.x = -0.2+0.02*cou;
-        vel.linear.y = 0.0;
+        vel.linear.x =  0.0;
+        vel.linear.y = -1*sokudo;
         vel.linear.z = 0.0;
         vel.angular.x = 0.0;
         vel.angular.y = 0.0;
         vel.angular.z = 0.0;
-        pub_twist.publish(vel);
-        if(cou>=10){
+        //pub_twist.publish(vel);
+        // if(cou>=40){
+       if(r_kyori<0.6){
+          printf("aaaaa=\n");
+          vel.linear.y = -0.1;
+        }
+       if(r_kyori<0.4 && r_kyori>0){
           cou=0;
-          stage++;
+          stage++;//強制終了
         }
         break;
       case 8:
-        vel.linear.x = 0.0;
-        vel.linear.y = 0.0;
+        cou++;
+        vel.linear.x =  0.0;
+        vel.linear.y = sokudo;
         vel.linear.z = 0.0;
         vel.angular.x = 0.0;
         vel.angular.y = 0.0;
         vel.angular.z = 0.0;
-        pub_twist.publish(vel);
-        fin.data=7;
-        pub_hand_task.publish(fin);
-        //if(robot_mode==0)stage=0;
+        //pub_twist.publish(vel);
+        // if(cou>=40){
+       if(l_kyori<0.7){
+          printf("aaaaa=\n");
+          vel.linear.y = 0.1;
+        }
+       if(l_kyori<0.4 && l_kyori>0){
+          cou=0;
+          stage=99;//強制終了
+        }
         break;
       case 99:
         vel.linear.x = 0.0;
@@ -166,11 +229,14 @@ int main(int argc, char **argv){
         vel.angular.y = 0.0;
         vel.angular.z = 0.0;
         pub_twist.publish(vel);
-        if(robot_mode==0)stage=0;
+        //if(robot_mode==0)stage=0;
         break;
     }
+    pub_twist.publish(vel);
     //if(robot_mode==0)stage=0;
-    if(robot_mode==2)stage=99;
+    //if(robot_mode==2)stage=99;
+    printf("state=%d\n",stage);
+    printf("lkyori:%f\n",l_kyori);
     ros::spinOnce();
     rate.sleep();
   }
