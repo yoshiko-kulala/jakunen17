196bce178733657e28a0d96cb3808601cc8b6850
Yano21210 <aichitead21210@gmail.com>
2022-07-18 13:40:05 +0900

    kadai1_18

--- src/kyori_de_tomaru.cpp
+++ src/kyori_de_tomaru.cpp
@@ -13,7 +13,8 @@ float f_kyori = -1;
 float b_kyori = -1;
 float r_kyori = -1;
 float l_kyori = -1;
-float sokudo = 0.2;
+float sokudo = 0.40;
+float gensoku= 0.1;
 
 void callback_range_front(const std_msgs::Float32& msg) {
     //ROS_INFO("/range_front\t:\t%f\n\n", msg.data);
@@ -74,11 +75,11 @@ int main(int argc, char **argv){
         vel.angular.y = 0.0;
         vel.angular.z = 0.0;
         // if(cou>=40){
-        if(f_kyori<0.6){
+        if(f_kyori<0.5){
           printf("aaaaa=\n");
-          vel.linear.x = 0.1;
+          vel.linear.x = gensoku;
         }
-        if(f_kyori<0.4 && f_kyori>0){
+        if(f_kyori<0.43 && f_kyori>0){
           cou=0;
           stage++;//強制終了
         }
@@ -94,11 +95,11 @@ int main(int argc, char **argv){
         vel.angular.z = 0.0;
         //pub_twist.publish(vel);
         // if(cou>=40){
-       if(r_kyori<0.6){
+       if(r_kyori<0.75){
           printf("aaaaa=\n");
-          vel.linear.y = -0.1;
+          vel.linear.y = -1*gensoku;
         }
-       if(r_kyori<0.4 && r_kyori>0){
+       if(r_kyori<0.45 && r_kyori>0){
           cou=0;
           stage++;//強制終了
         }
@@ -114,11 +115,11 @@ int main(int argc, char **argv){
         vel.angular.z = 0.0;
         //pub_twist.publish(vel);
         // if(cou>=40){
-        if(f_kyori<0.6){
+        if(f_kyori<0.7){
           printf("aaaaa=\n");
-          vel.linear.x = 0.1;
+          vel.linear.x = gensoku;
         }
-        if(f_kyori<0.4 && f_kyori>0){
+        if(f_kyori<0.45 && f_kyori>0){
           cou=0;
           stage++;//強制終了
         }
@@ -126,19 +127,19 @@ int main(int argc, char **argv){
         break;
     case 4:
         cou++;
-        vel.linear.x = 0.0;
-        vel.linear.y = sokudo;
+        vel.linear.x = sokudo;
+        vel.linear.y = 0.0;
         vel.linear.z = 0.0;
         vel.angular.x = 0.0;
         vel.angular.y = 0.0;
         vel.angular.z = 0.0;
         //pub_twist.publish(vel);
         // if(cou>=40){
-       if(l_kyori<0.6){
+       if(f_kyori<0.7){
           printf("aaaaa=\n");
-          vel.linear.y = 0.1;
+          vel.linear.x = gensoku;
         }
-       if(l_kyori<0.4 && l_kyori>0){
+       if(f_kyori<0.45 && f_kyori>0){
           cou=0;
           stage++;//強制終了
         }
@@ -153,9 +154,9 @@ int main(int argc, char **argv){
         vel.angular.y = 0.0;
         vel.angular.z = 0.0;
         // if(cou>=40){
-       if(l_kyori>1.3){
+       if(l_kyori>1.45){
           printf("aaaaa=\n");
-          vel.linear.y = -0.1;
+          vel.linear.y = -1*gensoku;
         }
        if(l_kyori>1.65 && l_kyori>0){
         printf("tikai\n");
@@ -174,30 +175,30 @@ int main(int argc, char **argv){
         vel.angular.z = 0.0;
         //pub_twist.publish(vel);
         // if(cou>=40){
-       if(b_kyori<0.6){
+       if(b_kyori<0.7){
           printf("aaaaa=\n");
-          vel.linear.x = -0.1;
+          vel.linear.x = -1*gensoku;
         }
-       if(b_kyori<0.4 && b_kyori>0){
+       if(b_kyori<0.45 && b_kyori>0){
           cou=0;
           stage++;//強制終了
         }
         break;
       case 7:
         cou++;
-        vel.linear.x =  0.0;
-        vel.linear.y = -1*sokudo;
+        vel.linear.x =  sokudo;
+        vel.linear.y = 0.0;
         vel.linear.z = 0.0;
         vel.angular.x = 0.0;
         vel.angular.y = 0.0;
         vel.angular.z = 0.0;
         //pub_twist.publish(vel);
         // if(cou>=40){
-       if(r_kyori<0.6){
+       if(r_kyori<0.55){
           printf("aaaaa=\n");
-          vel.linear.y = -0.1;
+          vel.linear.x = -1*gensoku;
         }
-       if(r_kyori<0.4 && r_kyori>0){
+       if(r_kyori<0.45 && r_kyori>0){
           cou=0;
           stage++;//強制終了
         }
@@ -214,9 +215,9 @@ int main(int argc, char **argv){
         // if(cou>=40){
        if(l_kyori<0.7){
           printf("aaaaa=\n");
-          vel.linear.y = 0.1;
+          vel.linear.y = gensoku;
         }
-       if(l_kyori<0.4 && l_kyori>0){
+       if(l_kyori<0.35 && l_kyori>0){
           cou=0;
           stage=99;//強制終了
         }
@@ -236,7 +237,10 @@ int main(int argc, char **argv){
     //if(robot_mode==0)stage=0;
     //if(robot_mode==2)stage=99;
     printf("state=%d\n",stage);
-    printf("lkyori:%f\n",l_kyori);
+    printf("F_kyori:%f\n",f_kyori);
+    printf("B_kyori:%f\n",b_kyori);
+    printf("R_kyori:%f\n",r_kyori);
+    printf("L_kyori:%f\n\n",l_kyori);
     ros::spinOnce();
     rate.sleep();
   }
